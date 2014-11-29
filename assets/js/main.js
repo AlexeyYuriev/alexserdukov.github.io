e7aca09ed5ce622d3a1e79604508eac66afe8de6
Oleksandr Serdiukov <alexserdukov@gmail.com>
2014-11-30 01:53:22 +0200

    Auction second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,53 @@
  * Created by alex on 26.11.14.
  */
 
-$(function(){
 
-    var productList = $("#products_list_template").html();
+function defaults(){
+    $("#search_template").hide();
+    $("#extra-search").hide();
+
+    var productList = $("#products_template").html();
     var template = Handlebars.compile(productList);
 
     $.getJSON("assets/data/featured-products.json", function (products) {
-        $("ul#products").append(template(products.items));
+        $("ul#templates").append(template(products.items));
     }).fail(function (jqXHR, textStatus) {
         console.log("Error code: " + jqXHR.status);
     });
+}
 
-    var searchList = $("#search_results_template").html();
+function activateSearch() {
+
+    $("#extra-search").show();
+    $(".search_template").show();
+
+    var searchList = $("#search_template").html();
     var searchTemplate = Handlebars.compile(searchList);
 
     $.getJSON("assets/data/search-results.json", function (results) {
-        $("ul#searchResults").append(searchTemplate(results.items));
+        $("ul#templates").append(searchTemplate(results.items));
     }).fail(function (jqXHR, textStatus) {
         console.log("Error code: " + jqXHR.status);
     });
 
-    $("#slider").on("slide", function(value){
+    $("#slider").on("slide", function (value) {
         $("#priceFrom").val(value.value[0]);
         $("#priceTo").val(value.value[1]);
     });
 
     $("#slider").slider({});
 
-    $("#searchResultsForm").hide();
+    $(".datepicker").datetimepicker();
+
+
+}
+
+$(".navbar-form").submit(function (event) {
+    $(".navbar-form").hide();
+    $(".carousel").hide();
+    $(".products_template").hide();
+    activateSearch();
+    event.preventDefault();
 });
 
-$("#navbar").on("click", "#searchButton", function() {
-    $("#featured-product-carousel").hide();
-    $("form#searchForm").hide();
-    $("#products").hide();
-    $("#searchResultsForm").show();
-
-});
+defaults();
