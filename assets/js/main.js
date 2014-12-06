e33411bb048a5783c34b25b934f49bd3a63de019
Oleksandr Serdiukov <alexserdukov@gmail.com>
2014-12-06 22:49:24 +0200

    rework of hw1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,52 +3,62 @@
  */
 
 
-function defaults(){
-    $("#search_template").hide();
-    $("#extra-search").hide();
+function defaults() {
 
-    var productList = $("#products_template").html();
-    var template = Handlebars.compile(productList);
-
-    $.getJSON("assets/data/featured-products.json", function (products) {
-        $("ul#templates").append(template(products.items));
-    }).fail(function (jqXHR, textStatus) {
-        console.log("Error code: " + jqXHR.status);
+    $.get("navbar.html", function (html) {
+        $("#navbar").append(html);
+        $(".navbar-form").submit(function () {
+            $(".navbar-form").hide();
+            $("#carousel").hide();
+            $(".products_template").hide();
+            activateSearch();
+            event.preventDefault();
+        });
     });
+
+    $.get("carousel.html", function (html) {
+        $("#carousel").append(html)
+    });
+
+    $.get("productTemplate.html", function (html) {
+        $.getJSON("assets/data/featured-products.json", function (products) {
+            var template = Handlebars.compile(html);
+            $("#templates").append(template(products.items));
+        }).fail(function (jqXHR, textStatus) {
+            console.log("Error code: " + jqXHR.status);
+        });
+    });
+
 }
 
 function activateSearch() {
 
-    $("#extra-search").show();
-    $(".search_template").show();
+    $.get("searchForm.html", function (html) {
+        $("#extra-search").append(html);
 
-    var searchList = $("#search_template").html();
-    var searchTemplate = Handlebars.compile(searchList);
+        var slider = $("#slider");
+        slider.on("slide", function (value) {
+            $("#priceFrom").val(value.value[0]);
+            $("#priceTo").val(value.value[1]);
+        });
+        slider.slider({});
 
-    $.getJSON("assets/data/search-results.json", function (results) {
-        $("ul#templates").append(searchTemplate(results.items));
-    }).fail(function (jqXHR, textStatus) {
-        console.log("Error code: " + jqXHR.status);
+        $(".datepicker").datetimepicker();
+
+        $("#extra-search-form").submit(function () {
+            event.preventDefault();
+        });
     });
 
-    $("#slider").on("slide", function (value) {
-        $("#priceFrom").val(value.value[0]);
-        $("#priceTo").val(value.value[1]);
+    $.get("searchTemplate.html", function (html) {
+        $.getJSON("assets/data/search-results.json", function (results) {
+            var searchTemplate = Handlebars.compile(html);
+            $("#templates").append(searchTemplate(results.items));
+            $(".current-time").find("span").text(moment().calendar());
+        }).fail(function (jqXHR, textStatus) {
+            console.log("Error code: " + jqXHR.status);
+        });
     });
-
-    $("#slider").slider({});
-
-    $(".datepicker").datetimepicker();
-
-
 }
-
-$(".navbar-form").submit(function (event) {
-    $(".navbar-form").hide();
-    $(".carousel").hide();
-    $(".products_template").hide();
-    activateSearch();
-    event.preventDefault();
-});
 
 defaults();
